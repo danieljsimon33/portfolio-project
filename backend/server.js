55b8b4133d065e2340ffbfc383c519a9979f7df9
lion4202 <brickbuilder2024@gmail.com>
2024-12-04 12:13:49 +0200

    dotenv setup

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
@@ -10,8 +12,14 @@ app.get("/default-route", (req, res) =>
   res.json({ message: "Connection established." })
 );
 
+// #prettier-ignore
+mongoose
+  .connect(process.env.MONGO_KEY)
+  .then(() => console.log("MongoDB connected."))
+  .catch((error) => console.log(error, "MongoDB connection attempt failed."));
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`App running on port -  ${PORT}`);
+  console.log(`App running on port ${PORT}`);
 });
