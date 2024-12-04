eb0d4c93cbea4b62e21a0bf1279bc2b1f44c7a40
lion4202 <brickbuilder2024@gmail.com>
2024-12-04 10:48:28 +0200

    portfolio-project

--- backend/server.js
+++ backend/server.js
@@ -13,5 +13,5 @@ app.get("/default-route", (req, res) =>
 const PORT = 3000;
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}`);
+  console.log(`App running on port -  ${PORT}`);
 });
