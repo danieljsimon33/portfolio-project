9829f2c0c2c8a7eecc78921062604e854c149de6
daniejsimon33 <danieljsimon33@gmail.com>
2024-12-05 13:53:00 +0200

    wrote pseudocode in guard.js and /router/authRouter.js that should finish up the auth process

--- backend/guard.js
+++ backend/guard.js
@@ -1,1 +1,18 @@
 // stuff
+
+/* 
+import jwt
+
+async func authenticateUser(req, res, next) {
+  split the req and extract the token
+
+  try {
+    !token ?
+      return access denied
+  
+    else
+      bool verif = jwt.verify(env key stuff)
+      next();
+  }
+}
+*/
--- backend/router/authRouter.js
+++ backend/router/authRouter.js
@@ -6,4 +6,6 @@ const router = express.Router();
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 
+// GET   "/validate-token", auth func from guard.js, func sending back validation message (this is the next function)
+
 module.exports = router;
