ee0a1dc3b47735bb55f72d7c1fdde670f0feefcb
daniejsimon33 <danieljsimon33@gmail.com>
2024-12-05 14:06:27 +0200

    added /validate-token to /router/authRouter.js

--- backend/router/authRouter.js
+++ backend/router/authRouter.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const userController = require("../controllers/user");
 
+const authenticateUser = require("../guard");
+
 const router = express.Router();
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
 
-// GET   "/validate-token", auth func from guard.js, func sending back validation message (this is the next function)
+router.get("/validate-token", authenticateUser, (req, res) =>
+  res.status(200).json({ message: "[Backend] Token verified." })
+);
 
 module.exports = router;
