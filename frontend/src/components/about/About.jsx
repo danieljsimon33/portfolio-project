a06037f3e8438019ed2e6810bead3c565364a191
daniejsimon33 <danieljsimon33@gmail.com>
2024-12-09 13:52:23 +0200

    updates to about me

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -1,11 +1,19 @@
 export default function About() {
+  /* prettier-ignore */
   return (
     <div className="page-container">
       <h2>About Me</h2>
-      {/* prettier-ignore */}
       <p>
-        My name is Daniel Simon and I am a high school graduate from the Charles E. Smith Jewish Day School.  [2024.12.09]
+        My name is Daniel Simon and I am a high school graduate set to study mechanical engineering next school year. My hobbies include 3d printing, drawing, reading, and playing Minecraft, and I enjoy a good mental challenge. [2024.12.09]
+      </p>
+      <p>
+        I also enjoy learning about new and developing technologies, especially in the consumer market.
       </p>
     </div>
   );
 }
+
+/* 
+consider adding a fun language change function- pirate mode, normal mode, etc.
+
+*/
