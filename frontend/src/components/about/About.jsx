26d98feb608c1e6a5f851991414611de02fdb4cc
daniejsimon33 <danieljsimon33@gmail.com>
2024-12-09 11:24:15 +0200

    adding content to about page

--- frontend/src/components/about/About.jsx
+++ frontend/src/components/about/About.jsx
@@ -1,3 +1,11 @@
 export default function About() {
-  return <div className="page-container">About</div>;
+  return (
+    <div className="page-container">
+      <h2>About Me</h2>
+      {/* prettier-ignore */}
+      <p>
+        My name is Daniel Simon and I am a high school graduate from the Charles E. Smith Jewish Day School.  [2024.12.09]
+      </p>
+    </div>
+  );
 }
