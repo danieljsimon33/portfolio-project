e631a3c4b404c5875bec8346ff7e35cc72a0d055
daniejsimon33 <danieljsimon33@gmail.com>
2024-12-11 12:12:14 +0200

    finished num to string conversion function for comp and player moves

--- frontend/src/components/projects/rock-paper-scissors/rps-functions.js
+++ frontend/src/components/projects/rock-paper-scissors/rps-functions.js
@@ -1,3 +1,16 @@
-export function updateGameText(playerMove, computerMove) {
-  return {};
+export function updateGameText(gameStats) {
+  return {
+    playerMove:
+      gameStats.playerMoveNum === 1
+        ? "rock"
+        : gameStats.playerMoveNum === 2
+        ? "paper"
+        : "scissors",
+    computerMove:
+      gameStats.computerMoveNum === 1
+        ? "rock"
+        : gameStats.computerMoveNum === 2
+        ? "paper"
+        : "scissors"
+  };
 }
