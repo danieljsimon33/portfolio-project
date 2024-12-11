30c444e74987d6fbd4764f8b39ef323625cd04ce
daniejsimon33 <danieljsimon33@gmail.com>
2024-12-11 11:59:34 +0200

    progress on rock paper scissors

--- frontend/src/components/projects/rock-paper-scissors/RockPaperScissors.jsx
+++ frontend/src/components/projects/rock-paper-scissors/RockPaperScissors.jsx
@@ -3,26 +3,54 @@ import { useState } from "react";
 export default function RockPaperScissors() {
   const [gameStats, setGameStats] = useState({
     isAutoPlaying: false,
-    score: {
+    scores: {
       wins: 0,
       losses: 0,
       ties: 0
     },
-    prevGameResult: null // this will usually be a number 1-3 that indicates win/loss/tie
+    gameResult: null // this will usually be a number 1-3 that indicates win/loss/tie
   });
 
   return (
     <div className="page-container">
       <h2>Rock Paper Scissors</h2>
+
       <div className="game-board">
         <button className="rock-button">
-          <img src="rps-images/rock-emoji.png" alt="" />
+          <img src="rps-images/rock-emoji.png" alt="rock" />
         </button>
+
         <button className="paper-button">
-          <img src="rps-images/paper-emoji.png" alt="" />
+          <img src="rps-images/paper-emoji.png" alt="paper" />
         </button>
+
         <button className="scissors-button">
-          <img src="rps-images/scissors-emoji.png" alt="" />
+          <img src="rps-images/scissors-emoji.png" alt="scissors" />
+        </button>
+
+        <p className="picks">
+          You picked: <img src="" alt="player move" />
+        </p>
+
+        <p className="results">
+          Result:
+          {gameStats.gameResult === null
+            ? ""
+            : gameStats.gameResult === 1
+            ? "you won"
+            : gameStats.gameResult === 2
+            ? "you lost"
+            : "tie"}
+        </p>
+
+        <p className="scores">
+          Wins: {gameStats.scores.wins} Losses: {gameStats.scores.losses}
+          Ties: {gameStats.scores.ties}
+        </p>
+
+        <button className="reset-button">Reset Game</button>
+        <button onclick="">
+          Auto Play: {gameStats.isAutoPlaying ? "ON" : "OFF"}
         </button>
       </div>
     </div>
