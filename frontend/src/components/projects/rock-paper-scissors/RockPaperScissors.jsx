6fbeb7763b2c1cf9ee8e9cc03d6dfb801e7bae2b
daniejsimon33 <danieljsimon33@gmail.com>
2024-12-11 11:18:21 +0200

    progress on RPS

--- frontend/src/components/projects/rock-paper-scissors/RockPaperScissors.jsx
+++ frontend/src/components/projects/rock-paper-scissors/RockPaperScissors.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function RockPaperScissors() {
-  return <div>RockPaperScissors</div>;
+  return (
+    <div className="page-container">
+      <h2>Rock Paper Scissors</h2>
+      <div className="game-board"></div>
+    </div>
+  );
 }
