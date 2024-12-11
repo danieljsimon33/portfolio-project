0b41ee1979e873ad955b825048395fb095ab77bf
daniejsimon33 <danieljsimon33@gmail.com>
2024-12-11 14:45:43 +0200

    game prolly works except it doesn't render bc too much stuff with react hooks

--- frontend/src/components/projects/rock-paper-scissors/RockPaperScissors.jsx
+++ frontend/src/components/projects/rock-paper-scissors/RockPaperScissors.jsx
@@ -2,48 +2,86 @@ import { useState } from "react";
 
 import "./RockPaperScissors.css";
 
+import {
+  numericalPlayGame,
+  computerMove,
+  gameScoresArray,
+  gameResultNumToText,
+  moveNumToText
+} from "./rps-functions.js";
+
 export default function RockPaperScissors() {
   const [gameStats, setGameStats] = useState({
     scores: {
       wins: 0,
       losses: 0,
       ties: 0
-    },
-    gameResult: null // this will usually be a number 1-3 that indicates win/loss/tie
+    }
   });
+
   const [gameStatsText, setGameStatsText] = useState({
     playerMoveText: null,
     computerMoveText: null,
     gameResultText: ""
   });
+
   const [isAutoPlaying, setIsAutoPlaying] = useState(false);
+
+  function handleSetAutoPlay() {
+    setIsAutoPlaying((prevValue) => !prevValue);
+  }
+
+  function handleSelectMove(moveNum) {
+    setGameStats((prevStats) => {
+      let newComputerMoveNum = computerMove();
+      let newGameResultNum = numericalPlayGame(moveNum, newComputerMoveNum);
+      let gameResultArray = gameScoresArray(newGameResultNum);
+
+      setGameStatsText(() => ({
+        playerMoveText: moveNumToText(moveNum),
+        computerMoveText: moveNumToText(newComputerMoveNum),
+        gameResultText: gameResultNumToText(newGameResultNum)
+      }));
+
+      return {
+        scores: {
+          wins: prevStats.scores.wins + gameResultArray[0],
+          losses: prevStats.scores.losses + gameResultArray[1],
+          ties: prevStats.scores.ties + gameResultArray[2]
+        }
+      };
+    });
+  }
 
   return (
     <div className="page-container">
       <h2>Rock Paper Scissors</h2>
 
       <div className="game-board">
-        <button className="rps-move-button">
+        <button className="rps-move-button" onClick={handleSelectMove(1)}>
           <img
             src="rps-images/rock-emoji.png"
             alt="rock"
             className="rps-move-image"
+            onClick={() => handleSelectMove(1)}
           />
         </button>
 
-        <button className="rps-move-button">
+        <button className="rps-move-button" onClick={handleSelectMove(2)}>
           <img
             src="rps-images/paper-emoji.png"
             alt="paper"
             className="rps-move-image"
+            onClick={() => handleSelectMove(2)}
           />
         </button>
 
-        <button className="rps-move-button">
+        <button className="rps-move-button" onClick={handleSelectMove(3)}>
           <img
             src="rps-images/scissors-emoji.png"
             alt="scissors"
             className="rps-move-image"
+            onClick={() => handleSelectMove(3)}
           />
         </button>
       </div>
@@ -72,16 +110,7 @@ export default function RockPaperScissors() {
           )}
         </p>
 
-        <p className="rps-info">
-          Results:
-          {gameStats.gameResult === null
-            ? ""
-            : gameStats.gameResult === 1
-            ? "you won"
-            : gameStats.gameResult === 2
-            ? "you lost"
-            : "tie"}
-        </p>
+        <p className="rps-info">Results: {gameStatsText.gameResultText}</p>
 
         <p className="rps-info">
           Wins: {gameStats.scores.wins} Losses: {gameStats.scores.losses} Ties:{" "}
@@ -90,10 +119,9 @@ export default function RockPaperScissors() {
       </div>
 
       <button className="reset-button">Reset Game</button>
-      <button onclick={() => setIsAutoPlaying((prevValue) => !prevValue)}>
+      <button onClick={() => handleSetAutoPlay}>
         Auto Play: {isAutoPlaying ? "ON" : "OFF"}
       </button>
     </div>
   );
 }
-// to do: decide where the game result text is set
--- frontend/src/components/projects/rock-paper-scissors/rps-functions.js
+++ frontend/src/components/projects/rock-paper-scissors/rps-functions.js
@@ -1,12 +1,43 @@
-export function updateGameText(playerMoveNum, computerMoveNum) {
-  return {
-    playerMove:
-      playerMoveNum === 1 ? "rock" : playerMoveNum === 2 ? "paper" : "scissors",
-    computerMove:
-      computerMoveNum === 1
-        ? "rock"
-        : computerMoveNum === 2
-        ? "paper"
-        : "scissors"
-  };
+export function computerMove() {
+  return Math.ceil(Math.random() * 3);
+}
+
+export function moveNumToText(num) {
+  return num === 1 ? "rock" : num === 2 ? "paper" : "scissors";
+}
+
+export function gameResultNumToText(num) {
+  return num === 1 ? "win" : num === 2 ? "loss" : "tie";
+}
+
+export function gameScoresArray(gameResultNum) {
+  let resultNumArray = [0, 0, 0];
+
+  switch (gameResultNum) {
+    case 1:
+      resultNumArray[0]++;
+    case 2:
+      resultNumArray[1]++;
+    case 3:
+      resultNumArray[2]++;
+  }
+
+  return resultNumArray;
+}
+
+export function numericalPlayGame(playerMoveNum, computerMoveNum) {
+  let moveDiff = playerMoveNum - computerMoveNum;
+
+  if (Math.abs(moveDiff) == 2) {
+    moveDiff = -moveDiff;
+  }
+
+  switch (Math.sign(moveDiff)) {
+    case 0:
+      return 3;
+    case -1:
+      return 2;
+    case 1:
+      return 1; //win
+  }
 }
