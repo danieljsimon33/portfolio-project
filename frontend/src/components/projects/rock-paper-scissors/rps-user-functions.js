e85a94ebcc77de63fd9e87cdde075ee25fbd2fbc
daniejsimon33 <danieljsimon33@gmail.com>
2024-12-18 10:39:57 +0200

    RPS user stuff

--- frontend/src/components/projects/rock-paper-scissors/rps-user-functions.js
+++ frontend/src/components/projects/rock-paper-scissors/rps-user-functions.js
@@ -1,1 +1,11 @@
-export async function updateStats() {}
+export async function updateStats() {
+  try {
+    const response = await fetch("http://localhost:3000", {
+      headers: {
+        "Content-Type": "applicaton/json"
+      }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
