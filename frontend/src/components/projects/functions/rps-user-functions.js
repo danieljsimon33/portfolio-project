cd7fca651c5c05cc97791715d944c846487edfc8
daniejsimon33 <danieljsimon33@gmail.com>
2024-12-19 14:38:49 +0200

    added frontend getStats and getLeaderboard functions

--- frontend/src/components/projects/functions/rps-user-functions.js
+++ frontend/src/components/projects/functions/rps-user-functions.js
@@ -10,7 +10,7 @@ export async function updateStats(userId, wins, losses, ties) {
         body: JSON.stringify({
           userId: userId,
           stats: { newWins: wins, newLosses: losses, newTies: ties }
-        }) //update the backend respectively
+        })
       }
     );
 
@@ -22,6 +22,59 @@ export async function updateStats(userId, wins, losses, ties) {
     const responseData = await response.json();
 
     console.log(responseData);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getStats(userId) {
+  try {
+    const response = await fetch(
+      "https://portfolio-project-hyba.onrender.com/get-stats",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "applicaton/json"
+        },
+        body: JSON.stringify({
+          userId: userId
+        })
+      }
+    );
+
+    if (!response.ok) {
+      const errorText = await response.json();
+      throw new Error(errorText);
+    }
+
+    const responseData = await response.json();
+
+    return responseData?.stats;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getLeaderboard() {
+  try {
+    const response = await fetch(
+      "https://portfolio-project-hyba.onrender.com/get-leaderboard",
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "applicaton/json"
+        }
+      }
+    );
+
+    if (!response.ok) {
+      const errorText = await response.json();
+      throw new Error(errorText);
+    }
+
+    const responseData = await response.json();
+
+    return responseData?.childArray;
   } catch (error) {
     console.log(error);
   }
