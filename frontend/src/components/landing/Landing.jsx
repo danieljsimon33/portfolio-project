94d30495500f8ccf6cc8d0c46a3fe9567a83ab9b
daniejsimon33 <danieljsimon33@gmail.com>
2024-12-29 19:21:14 -0500

    coloring

--- frontend/src/components/landing/Landing.jsx
+++ frontend/src/components/landing/Landing.jsx
@@ -3,12 +3,13 @@ export default function Landing() {
     <div className="page-container">
       <div className="about-me">
         <p>
-          Hi!  My name is Daniel Simon and I am a high school graduate set to study mechanical engineering next school year. My hobbies include drawing, 3D printing, reading, and playing Minecraft, and I enjoy a good mental challenge.
+          Hi!  My name is Daniel Simon and I am a high school graduate set to study mechanical engineering starting next school year, while currently I am taking a gap year with Aardvark Israel. My hobbies include drawing, 3D printing, reading, and playing Minecraft, and I enjoy a good mental challenge.
         </p>
         <p>
-          This small website is intended to demonstrate the skills I've learned throughout my full stack developer course taught by <a href="https://www.methodian.net" rel="external" target="_blank">Methodian</a>.
+          This small website is intended to demonstrate the skills I've learned throughout my full stack developer course taught by <a href="https://www.methodian.net" rel="external" target="_blank" className="methodian-link">Methodian</a>.
         </p>
       </div>
+
       <div className="about-course">
         <p>In this course, I learned how to implement:</p>
         <p className="parent-item">- User authentication</p>
--- frontend/src/components/navbar/NavBar.jsx
+++ frontend/src/components/navbar/NavBar.jsx
@@ -6,36 +6,36 @@ export default function NavBar() {
   return (
     <nav className="nav-bar">
       <ul className="nav-list">
-        <li>
+        <li className="nav-item">
           <Link className="nav-link" to="/">
             Home
           </Link>
         </li>
-        {/* <li>
+        {/* <li className="nav-item">
           <Link className="nav-link" to="/about">
             About Me
           </Link>
         </li> */}
-        <li>
+        <li className="nav-item">
           <Link className="nav-link" to="/projects">
             Projects
           </Link>
         </li>
         {!localStorage.getItem("token") ? (
           <>
-            <li>
-              <Link className="nav-link" to="/register">
-                Register
-              </Link>
-            </li>
-            <li>
+            <li className="nav-item">
               <Link className="nav-link" to="/login">
                 Log In
               </Link>
             </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/register">
+                Register
+              </Link>
+            </li>
           </>
         ) : (
-          <li>
+          <li className="nav-item">
             <button
               className="nav-button"
               onClick={() => {
